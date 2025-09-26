6ffb803835f23d85ade528205b62646c70286d12
xuantung159 <dxtung159@gmail.com>
2025-09-26 22:30:02 +0700

    'HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,20 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Đặng Xuân Tùng'\t'24020350'\t'2526I_INT2204_1'\t'xuantung159'\t'24020350@vnu.edu.vn");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.\n" +
+                "\n" +
+                "Take one down, pass it around, \n" +
+                "\n" +
+                "8 bottles of beer on the wall, 8 bottles of beer.\n" +
+                "\n" +
+                "Take one down, pass it around,\n" +
+                "\n" +
+                "...\n" +
+                "\n" +
+                "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                "\n" +
+                "Take one down, pass it around,\n" +
+                "\n" +
+                "No more bottles of beer on the wall.");
+    }
+}
